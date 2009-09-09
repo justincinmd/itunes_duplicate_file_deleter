73ff3260878747dd85276629a7e0538e54648a6b
root <root@jclinux.(none)>
2009-09-09 00:28:41 -0400

    compare files

--- detector.rb
+++ detector.rb
@@ -31,6 +31,35 @@ def remove_non_dupes
   }
 end
 
+def test_for_dupes
+  @files.each{|size, file_paths|
+    match_files = []
+
+    while file_paths.length > 0
+      base_file = file_paths[0]
+      match_files << base_file
+
+      for path in file_paths
+        unless match_files.include?(path)
+          match_files << path if File.compare(base_file, path)
+        end
+      end
+
+      clear_dupe(match_files)
+
+      file_paths = file_paths - match_files
+    end
+  }
+end
+
+def clear_dupe(matched_files)
+  puts ""
+  for file in matched_files
+    puts file
+  end
+  puts ""
+end
+
 puts "Scanning Directories"
 index_directory
 
@@ -38,3 +67,5 @@ puts "#{@total_files} Total Files Found"
 
 remove_non_dupes
 puts "#{@files.keys.length} Duplicate Sizes Found"
+
+puts "Testing for Dupes"
